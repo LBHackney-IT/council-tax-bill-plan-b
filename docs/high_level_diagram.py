ccfff510afff6e1941c77e1407a435f9000f5767
Wen Ting Wang <wenting@madetech.com>
2021-01-08 09:40:45 +0000

    Add script to output high-level diagram for database sources

---
+++ docs/high_level_diagram.py
@@ -0,0 +1,18 @@
+from diagrams import Diagram
+from diagrams.aws.database import Aurora
+from diagrams.generic.database import SQL
+from diagrams.generic.storage import Storage
+
+graph_attr = {
+    "nodesep": "1.00"
+}
+
+with Diagram("Hackney Council Tax Billing Plan B", show=True, direction="TB", graph_attr=graph_attr, filename="high_level_diagram"):
+    Aurora("POC Data") >> [
+        Storage("AWS Academy Recovery\n(October 2020)"),
+        Storage("FDM Bill Print Files\n(March 2020)"),
+        Storage("FDM Exemption Print Files\n(March 2020)"),
+        Storage("Working Age\n(August 2020)"),
+        Storage("VO Bands\n(January 2021)"),
+        Storage("Direct Debit\nDiscrepancies")
+    ]
